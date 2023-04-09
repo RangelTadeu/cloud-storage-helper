885d60cce437e71737253cd971cdf020dcff7a39
Tadeu Rangel <trangel.dev@gmail.com>
2023-04-09 19:13:30 -0300

    refact: change api to work with fastify

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,29 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
+import compression from '@fastify/compress';
+import helmet from '@fastify/helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      skipMissingProperties: true,
+    }),
+  );
+
+  await app.register(helmet);
+  await app.register(compression);
+
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
